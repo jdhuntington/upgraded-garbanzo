d4f00bd8d716117d3332d5de75305a457d2bbecd
JD Huntington <jdh@microsoft.com>
2016-04-27 15:34:03 -0700

    Use -searchdirs flag to identify pdb locations.

--- Run-Coverage.ps1
+++ Run-Coverage.ps1
@@ -5,4 +5,4 @@ Write-Host -ForegroundColor Cyan "This executes unit tests with xUnit..."
 .\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe CoverageInvestigation.Test\bin\Debug\CoverageInvestigation.Test.dll
 
 Write-Host -ForegroundColor Cyan "This fails to show any code has run..."
-.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"packages\xunit.runner.console.2.1.0\tools\xunit.console.exe" -targetargs:"CoverageInvestigation.Test\bin\Debug\CoverageInvestigation.Test.dll" -output:"nocoverage.xml" 
+.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"packages\xunit.runner.console.2.1.0\tools\xunit.console.exe" -targetargs:"CoverageInvestigation.Test\bin\Debug\CoverageInvestigation.Test.dll" -output:"nocoverage.xml" -searchdirs:".\CoverageInvestigation.Test\bin\Debug" -filter:"+[CoverageInvestigation]*"
